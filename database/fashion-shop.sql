14a44a5a0cba937ea6c4252142782e6a9dfb8ac8
Van Dao <van.dao@kiss-concept.com>
2013-08-02 21:38:53 +0700

    update database

--- database/fashion-shop.sql
+++ database/fashion-shop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 02, 2013 at 09:26 PM
+-- Generation Time: Aug 02, 2013 at 10:30 PM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.1
 
